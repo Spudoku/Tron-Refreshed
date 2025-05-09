c0d8da725a614ef65ed988b5ed995491277f5ca9
suspiciouslyBee <suspiciously@bee.movie>
2025-05-09 11:57:52 -0700

    trying hard to get this working

--- Assets/Scripts/Player/CameraControl.cs
+++ Assets/Scripts/Player/CameraControl.cs
@@ -1,16 +1,34 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+
+//largely stolen from dave/gamedevelopment yt channel (thanks!)
+//also sort of adapted from the lukeskt's implementation
 public class CameraControl : MonoBehaviour
 {
+    [SerializeField]
+    private InputActionAsset _actions;
+
+    public float sensX;
+    public float sensY;
+
+    public Transform orientation;
+
+    float xRotation;
+    float yRotation;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    //lock the cursor to the center, hide it becuase first person
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //float mouseX = Input.GetAxisRaw("")
     }
 }
