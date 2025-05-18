f14dc9bcb8e50b9c2cad0aadbc73c94d5c386d96
Spudoku <calebisntshort@gmail.com>
2025-05-18 14:48:41 -0700

    Adding UI Manager Adding Player Name Adding Menu Camera
    But the menu doesn't work right now

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,45 @@
+using TMPro;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.UI;
+
+// Code from this tutorial: https://www.youtube.com/watch?v=SZjpm950g_c
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] public TMP_InputField nameInputField;
+    [SerializeField] public TextMeshProUGUI gameInfoText;
+    [SerializeField] Button hostButton;
+    [SerializeField] Button joinButton;
+    [SerializeField] GameObject menu;
+
+    [SerializeField] Camera menuCam;
+
+    void Start()
+    {
+        hostButton.onClick.AddListener(Host);
+        joinButton.onClick.AddListener(Join);
+        menuCam.enabled = true;
+        menuCam.GetComponent<AudioListener>().enabled = false;
+        Debug.Log("[UIManager] Starting!");
+
+    }
+
+    void Host()
+    {
+        NetworkManager.Singleton.StartHost();
+        menu.SetActive(false);
+        menuCam.enabled = false;
+        gameInfoText.gameObject.SetActive(true);
+        gameInfoText.text = "Press Enter to Start";
+    }
+
+    void Join()
+    {
+        NetworkManager.Singleton.StartClient();
+        menu.SetActive(false);
+        menuCam.enabled = false;
+        gameInfoText.gameObject.SetActive(true);
+        gameInfoText.text = "Waiting for Host...";
+
+    }
+}
