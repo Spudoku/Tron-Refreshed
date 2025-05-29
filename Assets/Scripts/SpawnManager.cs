1736e0463d510ec20225cfad53dfc522263d95b5
Spudoku <calebisntshort@gmail.com>
2025-05-29 16:19:39 -0700

    Spawn system test

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] private GameObject[] spawnPoints;
+    [SerializeField] private GameObject[] jailPoints;
+    private static SpawnManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        spawnPoints = GameObject.FindGameObjectsWithTag("SpawnPoint");
+        jailPoints = GameObject.FindGameObjectsWithTag("JailBox");
+        Debug.Log("[SpawnManager] initialized!");
+    }
+
+    private void TeleportTo(GameObject go, GameObject location)
+    {
+        if (location == null)
+        {
+            Debug.Log("[TeleportTo] teleport failed: no valid location points");
+            return;
+        }
+        else if (go == null)
+        {
+            Debug.Log("[TeleportTo] teleport failed: invalid teleporting object");
+        }
+        go.transform.position = location.transform.position;
+    }
+
+    public void TeleportToRandomSpawnPoint(GameObject go)
+    {
+        int index = Random.Range(0, spawnPoints.Length);
+
+        TeleportTo(go, spawnPoints[index]);
+    }
+
+    public void TeleportToJail(GameObject go)
+    {
+        int index = Random.Range(0, jailPoints.Length);
+
+        TeleportTo(go, jailPoints[index]);
+    }
+
+
+}
